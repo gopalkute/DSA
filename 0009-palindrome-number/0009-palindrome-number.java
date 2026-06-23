ac0b1ef3ded9d5d3c6f99cf4f6675f04d4f9da98
Gopal Kute <gopalkute02@gmail.com>
2026-06-23 08:18:27 +0530

    Time: 4 ms (100%), Space: 45.7 MB (93.84%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+
+        if(x < 0) return false;
+
+        int original = x;
+        int rev = 0;
+
+        while(x > 0){
+            int rem = x % 10;
+            rev = rev * 10 + rem;
+            x = x / 10;
+        }
+
+        if(original != rev) return false;
+
+        return true;
+    }
+}
