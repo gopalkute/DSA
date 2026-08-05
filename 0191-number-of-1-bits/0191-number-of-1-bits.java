60943446b2009dea5a50463ccd5d4c25838947a4
Gopal Kute <gopalkute02@gmail.com>
2026-08-05 15:23:22 +0530

    Time: 0 ms (100%), Space: 42.5 MB (28.9%) - LeetHub

---
+++ 0191-number-of-1-bits/0191-number-of-1-bits.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int hammingWeight(int n) {
+        int bits = 0;
+        while(n > 0){
+            n = n & (n - 1);
+            bits++;
+        }
+
+        return bits;
+    }
+}
